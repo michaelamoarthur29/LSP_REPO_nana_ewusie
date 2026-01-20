9362e5bd171f3b1cc948811bfb0ece82c841b72a
mikeyamo-arthur <mikeyamo-arthur@localhost>
2026-01-19 20:35:18 -0500

    This is my first commit.

---
+++ LSP_Spring_2026/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	/**
+	 * Name: Michael Ewusi Amo-Arthur 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+
+	}
+
+}
